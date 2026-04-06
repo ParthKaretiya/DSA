5de960b948c2777aef58af39d1ceeb91c075e6fd
ParthKaretiya <parth.karetiya.cg@gmail.com>
2026-04-06 13:29:42 +0530

    initial commit

---
+++ lec07.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    // * 
+    // * * 
+    // * * * 
+    // * * * * 
+    // * * * * * 
+
+    // for(int i=1;i<=5;i++){
+    //     for(int j=1 ; j<=i ; j++){
+    //         cout<< "*" << " ";
+
+    //     }
+    //     cout<<endl;
+    // }  
+
+
+
+
+    // 1 
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+
+    //  for(int i=1;i<=5;i++){
+    //     for(int j=1 ; j<=i ; j++){
+    //         cout<< j << " ";
+
+    //     }
+    //     cout<<endl;
+    // }  
+
+
+
+    // 1 
+    // 2 2
+    // 3 3 3
+    // 4 4 4 4
+    // 5 5 5 5 5
+
+    //  for(int i=1;i<=5;i++){
+    //     for(int j=1 ; j<=i ; j++){
+    //         cout<< i << " ";
+
+    //     }
+    //     cout<<endl;
+    // } 
+
+
+    // 1 
+    // 2 1
+    // 3 2 1
+    // 4 3 2 1
+    // 5 4 3 2 1
+    
+    //  for(int i=1;i<=5;i++){
+    //     for(int j=i ; j>=1 ; j--){
+    //         cout<< j << " ";
+
+    //     }
+    //     cout<<endl;
+    // } 
+
+    // A 
+    // B B
+    // C C C
+    // D D D D
+    // E E E E E
+
+    // char c ='A';
+    // for(int i=1 ; i<=5 ; i++){
+    //     for(int j=i ; j>=1 ; j--){
+    //         cout << c << " ";
+    //     }
+    //     c++;
+    //     cout << endl;
+    // }
+
+
+    // * * * * * 
+    // * * * * 
+    // * * * 
+    // * * 
+    // * 
+
+    // for(int i=5 ; i>=1 ; i--){
+    //     for(int j=i ; j>=1 ; j--){
+    //         cout<< "*" << " ";
+    //     }
+    //     cout << endl ;
+    // }
+
+
+    // 1 2 3 4 5 
+    // 1 2 3 4 
+    // 1 2 3 
+    // 1 2 
+    // 1 
+
+    // for(int i=5 ; i>=1 ; i--){
+    //     for(int j=1 ; j<=i ; j++){
+    //         cout<< j << " ";
+    //     }
+    //     cout << endl ;
+    // }
+
+
+    // 5 
+    // 5 4
+    // 5 4 3
+    // 5 4 3 2
+    // 5 4 3 2 1
+
+    //     int n = 5;
+
+    //     for(int i=1;i<=5;i++){
+    //     for(int j=0 ; j<i ; j++){
+    //         cout<< n -j << " ";
+
+    //     }
+    //     cout<<endl;
+    // } 
+
+
+
+    return 0;
+}
